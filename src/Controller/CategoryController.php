0b5082dcb633ef56c47be4d7c7aa3c2d54d39120
Dorzal <vhelyno@gmail.com>
2018-12-20 00:43:16 +0100

    try

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -7,41 +7,18 @@
  */
 
 namespace App\Controller;
-//group
-use App\Entity\User;
-use App\Form\UserType;
+
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class CategoryController extends AbstractController
 {
-    /**
-     * @Route("/interest/{$this->token_storage->getToken()->getUser()->getId()}", name="interest")
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function interest(Request $request, User $user)
-    {
-        $em = $this->getDoctrine()->getManager();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $em->persist($user);
-            $em->flush();
-        }
-
-        return new Response(
-            $this->renderView(
-                'register/interest.html.twig',
-                array(
-                    'form' => $form->createView(),
-                )
-            )
-        );
+    public function interest(EntityManagerInterface $em){
+        $category = $em->getRepository(Category::class)->findAll();
+        return $this->render('Register/interest.html.twig', [
+            'category' => $category,
+        ]);
     }
+
 }
