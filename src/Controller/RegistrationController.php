78791171075a9655e2183a90d2c1d31db979ca7d
arnaudlinder <arnaud.linder@lacooperativewelcoop.com>
2019-01-02 11:09:31 +0100

    registration redirection choice interest

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -54,7 +54,7 @@ class RegistrationController extends AbstractController
             $this->container->get('security.token_storage')->setToken($token);
             $this->container->get('session')->set('_security_main', serialize($token));
 
-            return $this->redirectToRoute('all_product');
+            return $this->redirect('/interest/'.$user->getId());
         }
 
         return $this->render(
